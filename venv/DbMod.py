7aa6f0a1320846d9cb7269b86d2ed7d115f0c31b
szwagiers <mariusz_skowronski@wp.pl>
2021-07-10 14:57:24 +0200

    No crypto version. Added log in function

--- venv/DbMod.py
+++ venv/DbMod.py
@@ -1,19 +1,17 @@
 import sqlite3
-from encyption import *
+from encryption import *
 from colorama import Fore, Back, Style
 
-#create database for user/connect to databes
+#create database for user/connect to databese
 userDB = sqlite3.connect('Users.db')
 
 #set cursor
 cur = userDB.cursor()
 
 #create table for users data
-cur.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY,login TEXT UNIQUE ,password TEXT)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS users (id ,login TEXT UNIQUE ,password TEXT)''')
 userDB.commit()
-#create table for storing password data
-cur.execute('''CREATE TABLE IF NOT EXISTS pswrds (id INTEGER PRIMARY KEY,keyVal text,FOREIGN KEY (id) REFERENCES users(id))''')
-userDB.commit()
+
 
 #cur.execute(("SELECT * FROM {}").format(Tname))
 #userDB.commit()
@@ -23,29 +21,28 @@ userDB.commit()
 
 
 def insData(log,pswrd):
-    k,p= encrypt(pswrd)
-    cur.execute('''INSERT INTO users(login,password) VALUES ({},{})'''.format(log,p))
+    cur.execute('''INSERT INTO users(login,password) VALUES ('{}','{}')'''.format(log,pswrd))
     userDB.commit()
-    cur.execute('''INSERT INTO pswrds(keyVal) VALUES({})'''.format(str(k)))
 
 
-def selUsrData():
-    cur.execute('''SELECT * FROM users''')
+def selUsrData(q):
+    cur.execute('''{}'''.format(q))
+    print(cur.fetchall())
     for row in cur:
-        print('id=', row[0])
-        print('login=', row[1])
-        print('password=', row[2])
+        print('id=', row[0],'login=', row[1],'password=', row[2])
+        # print('login=', row[1])
+        # print('password=', row[2])
 
 
 def checkLogIn(log,pswrd):
-    cur.execute('''SELECT u.id,login,password,keyVal  FROM users as u JOIN pswrds as p on u.id=p.id ''')
+    cur.execute('''SELECT *  FROM users''')
     userDB.commit()
-    check=cur.fetchall()
-    c_suite = Fernet(check[3])
-    dePsw=c_suite.decrypt(check[2])
-    dbLog=check[1]
-    if log==dbLog and pswrd==dePsw:
-        print(Fore.GREEN,'Logged succesfully')
+    check=cur.fetchone()
+    if log in check and pswrd in check:
+    # if log==dbLog and pswrd==dePsw:
+        print(Fore.GREEN,'Logged in successfully')
+        print(Style.RESET_ALL)
 
 # cur.execute('''SELECT * FROM users''')
 # print(userDB.fetchall())
+
--- venv/UserMenuMod.py
+++ venv/UserMenuMod.py
@@ -1,26 +1,38 @@
 from Req_Mod import *
 from DbMod import *
-from encyption import *
+from encryption import *
+from colorama import Fore, Back, Style
 
 #Create user data
 def cr_user(log,pswrd):
     user={'Login':log,'passowrd':pswrd}
     return user
 
-def usrMenu():
+def usrMenu(web):
     ans=input('Select User or Admin. ').upper()
     if ans=='A':
+        print('Logged in successfully')
         #AdL, AdP=input('Please provide your login and password')
-        pass
+
+        Ops=input('Options: (I)nsertData,(S)electData ').upper()
+        if Ops == 'S':
+            Query=input('Please insert query ')
+            try:
+                selUsrData(Query)
+            except SyntaxError:
+                print('Please check your query')
+
     elif ans == 'U':
-        Uans =input('Are You a new user or do You want to login? New/Login ').upper()
+        Uans =input('Are you a new user or do you want to login? New/Login ').upper()
         if Uans == 'N':
             # Insert data from user
             l, p = input('Please provide your login and password (separete with space): ').split()
-            encrypt(p)
+            # encrypt(p)
             u = cr_user(l, p)
-            addUser(u)
+            addUser(web,u)
             insData(l,p)
+            print('Please log into your new account.')
+            usrMenu(web)
         elif Uans =='L':
             log,pswrd = input('Please provide Your login and password. ').split()
             checkLogIn(log,pswrd)
@@ -28,4 +40,4 @@ def usrMenu():
             pass
     else:
         pass
-    usrMenu()
+    usrMenu(web)
